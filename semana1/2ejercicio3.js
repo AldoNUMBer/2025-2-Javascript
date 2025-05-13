ba52c9e52c93d79f9fe9a689c7f5f3de42fa20e0
AldoNUMBer <cuevasaldo943@gmail.com>
2025-05-12 21:50:29 -0600

    ejersicio de clase 3

--- semana1/2ejercicio3.js
+++ semana1/2ejercicio3.js
@@ -4,22 +4,15 @@ const { ask } = require('../helpers/input');
 
 async function main() {
  const dia = Number(await ask("Ingresa número del 1 al 7"));
- const diasemana = ("Lunes", "Martes", "Miercoles", "Jueves", "Viernes")
+ const diasemana = any ("Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Viernes")
 
- if (dia >=1 && dia <= 7){
-    console.log(diasemana{dia - 1});
- } else {
+ if (dia => 1 && dia <= 7){
+    console.log(diasemana[dia - 1]);
+ } else if ( dia => 8)
     console.log("No es un numero válido");
  }
+main();
 
-
-
-
-
-
-}
-
- /* bla bla bla
- */  
+ // bla bla bla 
 
  //bla bla bla bla
